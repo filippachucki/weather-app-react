3fbd533f47745dd40b556a033f4dcc4e42b45900
Filip Pachucki <filippachuckii2000@gmail.com>
2022-10-28 07:48:46 +0100

    initial

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5429233b39mshf5b69dcf2caaac2p12250ejsn45d9db74080c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
